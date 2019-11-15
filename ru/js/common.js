8ed7b5e1aee823fc6893c172cdbd98c3f99c394d
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-11-15 17:23:26 +0300

    c

---
+++ ru/js/common.js
@@ -0,0 +1,59 @@
+// ========================>
+// slider for reviews
+// <=======================
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.sect7__pagination',
+    clickable: true,
+  },
+  // loop: true,
+  autoHeight: true,
+  spaceBetween: 40
+});
+// ========================>
+// animation scroll
+// <=======================
+(function () {
+  var linkNav = document.querySelectorAll('[href^="#"]'),
+    V = .2;
+  for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var w = window.pageYOffset,
+        hash = this.href.replace(/[^#]*(.*)/, '$1');
+      t = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+      requestAnimationFrame(step);
+      function step(time) {
+        if (start === null) start = time;
+        var progress = time - start,
+          r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+        window.scrollTo(0, r);
+        if (r != w + t) {
+          requestAnimationFrame(step)
+        } else {
+          location.hash = hash
+        }
+      }
+    }, false);
+  }
+})();
+// ========================>
+// validate input
+// <=======================
+(function () {
+  var input = document.querySelector('input[name="name"]');
+  input.addEventListener('input', function () {
+    input.value = input.value.replace(/[0-9]/g, '')
+  })
+})();
+// ========================>
+// pulse btn
+// <=======================
+(function () {
+  var offer = document.getElementById('offer');
+  var btn = document.querySelector('.pulse__btn');
+  window.addEventListener('scroll', (e) => {
+    document.documentElement.scrollTop + 300 > offer.offsetTop ? btn.classList.add('show') : btn.classList.remove('show')
+  })
+})();
